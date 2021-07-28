7744c7983e4c570a5d65c88bd862859467686ff8
Jon Koenig <jkoenig@outlook.com>
2021-07-27 19:36:21 -0600

    Refactoring.

--- Dagger.Services/Store.cs
+++ Dagger.Services/Store.cs
@@ -9,9 +9,13 @@ namespace Dagger.Services
     public static class Store
     {
         // Collections should not be implemented this way, but I'm just testing things out for now! :)
-        public static Dictionary<string, List<Dictionary<string, string>>> Collections { get; } =
-            new Dictionary<string, List<Dictionary<string, string>>>();
+        public static Dictionary<string, List<Dictionary<string, string>>> Collections { get; }
         
         public static List<Writable> Writable { get; } = new List<Writable>();
+
+        static Store()
+        {
+            Collections = new Dictionary<string, List<Dictionary<string, string>>>();
+        }
     }
 }
