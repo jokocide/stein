dfbf89a7dd3934d1a0441d2b73a2f424c1b9d460
Jon Koenig <jkoenig@outlook.com>
2021-07-29 20:31:45 -0600

    Delete test directory

--- test/resources/collections/posts/hello.md
+++
@@ -1,7 +0,0 @@
----
-template: post
-title: Hello, World!
-description: First post!
-date: 2020-12-04
----
-Not much to see here.
--- test/resources/public/js/main.js
+++
@@ -1,6 +0,0 @@
-function sendToRoot() {
-    console.log('moved to root');
-    window.location.href = "/";
-}
-
-document.getElementById('nav-branding-container').addEventListener('click', sendToRoot);
