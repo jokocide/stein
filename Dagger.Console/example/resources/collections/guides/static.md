6b465e72a560681564a893d52f5f846af57cb2c2
Jon Koenig <jkoenig@outlook.com>
2021-08-01 23:29:27 -0600

    Bugfix.
    Resolved an issue with the Writable constructor that resulted in
    all collection files receiving the wrong output directory.

--- Dagger.Console/example/resources/collections/guides/static.md
+++ Dagger.Console/example/resources/collections/guides/static.md
@@ -1,5 +1,5 @@
 ﻿---
-template: guides
+template: detail
 title: Static Files
 description: Where do static files like JavaScript and images go?
 date: 2021-08-01
--- Dagger.Data/Models/Writable.cs
+++ Dagger.Data/Models/Writable.cs
@@ -5,49 +5,72 @@ namespace Dagger.Data.Models
 {
     public class Writable
     {
-        private string ResourcePath { get; }
         public string SitePath { get; }
         public string Body { get; }
 
         public Writable(string resourcePath, string body)
         {
-            ResourcePath = resourcePath;
             Body = body;
             SitePath = MakeSitePath(resourcePath);
         }
 
         private string MakeSitePath(string path)
         {
-            string fileName = Path.GetFileNameWithoutExtension(path);
-            string directoryName = Path.GetDirectoryName(path);
-            string parentOfDirectoryName = Path.GetDirectoryName(directoryName);
-
-            if (directoryName == "pages" && parentOfDirectoryName != "collections")
-                switch (fileName)
-                {
-                    case "index":
-                        return Path.Join(Directory.GetCurrentDirectory(), "site", fileName + ".html");
-                    default:
-                        return MakeNonIndexPagePath(fileName);
-                }
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
             
-            if (parentOfDirectoryName != "collections")
-                throw new Exception($"Unable to generate writable path for: {path}");
+            string fileName = Path.GetFileNameWithoutExtension(directoryInfo.Name);
+            string directoryName = directoryInfo.Parent?.Name;
 
-            return MakeCollectionPath(fileName, directoryName);
+            // Todo: This switch track needs more error handling, and we probably shouldn't default to collection file.
+            return directoryName switch
+            {
+                // Resolving an index page file.
+                "pages" when fileName == "index" => Path.Join(Directory.GetCurrentDirectory(), "site", "index.html"),
+                // Resolving a non-index page file.
+                "pages" => MakeNonIndexPagePath(fileName),
+                // Resolving a collection file. 
+                _ => MakeCollectionPath(fileName, directoryName)
+            };
+            
         }
 
+        /// <summary>
+        /// Return a path suitable for writing a page file that isn't already an index file to the file system.
+        /// </summary>
+        /// <param name="fileName">The name of the file. Should not include a file extension.</param>
+        /// <returns>
+        /// A string path representing a suitable location for this file within a Dagger project's site directory.
+        /// </returns>
         private string MakeNonIndexPagePath(string fileName) 
         {
             string newDirectory = Path.Join(Directory.GetCurrentDirectory(), "site", fileName);
+            
+            // Assert the directory exists.
             Directory.CreateDirectory(newDirectory);
+
+            // Console.WriteLine(newDirectory, "index.html");
             return Path.Join(newDirectory, "index.html");
         }
 
-        private string MakeCollectionPath(string fileName, string directoryName)
+        /// <summary>
+        /// Return a path suitable for writing a collection file to the file system.
+        /// </summary>
+        /// <param name="fileName">The name of the file. Should not include a file extension.</param>
+        /// <param name="directoryName">
+        /// The name of the directory that the file resides in, or name of the collection that this file belongs to.
+        /// </param>
+        /// <returns>
+        /// A string path representing a suitable location for this file within a Dagger project's site directory.
+        /// </returns>
+        private string MakeCollectionPath(string fileName, string directoryName) // collections, guides
         {
+            // Console.WriteLine(Path.Join(Directory.GetCurrentDirectory(), "site", directoryName, fileName));
             string newDirectory = Path.Join(Directory.GetCurrentDirectory(), "site", directoryName, fileName);
+            
+            // Assert the directory exists.
             Directory.CreateDirectory(newDirectory);
+            
+            // Console.WriteLine(Path.Join(newDirectory, "index.html"));
             return Path.Join(newDirectory, "index.html");
         }
     }
