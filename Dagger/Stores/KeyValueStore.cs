2b0bf16e7e6e79f57851003720479cc21c2e9c74
Jon Koenig <jkoenig@outlook.com>
2021-08-07 21:35:08 -0600

    Tracking /Stores.

---
+++ Dagger/Stores/KeyValueStore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Dagger.Models;
+
+namespace Dagger.Storage
+{
+    /// <summary>
+    /// A basic storage scheme that allows for storing key value pairs.
+    /// </summary>
+    public sealed class KeyValueStore : Store
+    {
+        internal Dictionary<string, string> Pairs { get; }
+    }
+}
