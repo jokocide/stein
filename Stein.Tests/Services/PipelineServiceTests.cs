520e9c24b8927519421ec9968b3164df037d39b7
Jon Koenig <jkoenig@outlook.com>
2021-09-05 16:51:32 -0600

    Added PipelineServiceTests

---
+++ Stein.Tests/Services/PipelineServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Stein.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stein.Routines;
+
+namespace Stein.Services.Tests
+{
+    public class PipelineServiceTests
+    {
+        public void EvaluateTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Stein.Tests/Services/YamlServiceTests.cs
+++ Stein.Tests/Services/YamlServiceTests.cs
@@ -1,5 +1,6 @@
 ﻿using Xunit;
 using System.Collections.Generic;
+using System;
 
 namespace Stein.Services.Tests
 {
@@ -21,14 +22,33 @@ namespace Stein.Services.Tests
         /// the location of YAML frontmatter within a string, defaults to (0, 0, 0, 0)
         /// to indicate a problem with the format of the frontmatter, or that frontmatter
         /// does not exist in the string.
-        /// <param name="text">The source string.</param>
-        /// <param name="expectedResult">The expected tuple result.</param>
+        /// <param name="text">
+        /// The source string.
+        /// </param>
+        /// <param name="expectedResult">
+        /// The expected tuple result.
+        /// </param>
         [Theory]
         [MemberData(nameof(Indices))]
         public void GetIndicesTest(string text, (int, int, int, int) expectedResult)
         {
             (int, int, int, int) result = YamlService.GetIndices(text);
             Assert.Equal(expectedResult, result);
+        }
+
+        /// <summary>
+        /// Deserialize should return a dictionary by splitting up the string line by line at the 
+        /// first occurance of a given delimiter, which is ':' by default, and trimming all existing 
+        /// white space around the key/value pairs.
+        /// </summary>
+        [Fact()]
+        public void DeserializeTest()
+        {
+            string text = $"keyOne: Value for key one{Environment.NewLine}keyTwo: and value for key two is here.";
+            Dictionary<string, string> result = YamlService.Deserialize(text);
+
+            Assert.Equal("Value for key one", result["keyOne"]);
+            Assert.Equal("and value for key two is here.", result["keyTwo"]);
         }
     }
 }
