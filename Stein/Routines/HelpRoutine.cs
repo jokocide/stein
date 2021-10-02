b37970e5e8a14052686e23b0a9f81477249f17c3
Jon Koenig <jkoenig@outlook.com>
2021-10-02 10:25:44 -0600

    Moved logic from HelpRoutine.Execute into private methods.

--- Stein/Routines/HelpRoutine.cs
+++ Stein/Routines/HelpRoutine.cs
@@ -19,72 +19,13 @@ namespace Stein.Routines
         public override void Execute()
         {
             if (Topic is HelpTopic.Build)
-            {
-                Colorize("Description:", Gray, true);
-                Colorize("Attempt to process all resources in a given project and output the "
-                    + "results to the site directory.", White, true);
-                Colorize("Synopsis: ", Gray, true);
-                Colorize("build ", White, false);
-                Colorize("[path]", Gray, true);
-                Colorize("Example: ", Gray, true);
-                Colorize("stein build ", White, false);
-                Colorize("./projects/mysite", Gray, true);
-                Colorize("Options:", Gray, true);
-                Colorize("[path] ", White, false);
-                Colorize("(default: current directory)", DarkGray, true);
-                Colorize("An absolute or relative path to the project that is being built.", Gray, true);
-            }
+                PrintHelpTopicBuild();
             else if (Topic is HelpTopic.New)
-            {
-                Colorize("Description:", Gray, true);
-                Colorize("Scaffold out resources and site directories, as well as a " +
-                    "stein.json file in the root of the new project directory.", White, true);
-                Colorize("Synopsis: ", Gray, true);
-                Colorize("new ", White, false);
-                Colorize("[path] ", Gray, true);
-                Colorize("Example: ", Gray, true);
-                Colorize("stein new ", White, false);
-                Colorize("./projects/newsite", Gray, true);
-                Colorize("Options:", Gray, true);
-                Colorize("[path] ", White, false);
-                Colorize("(default: current directory)", DarkGray, true);
-                Colorize("An absolute or relative path to the desired location of the new project. " +
-                               "If the directory does not exist, it will be created.", Gray, true);
-            }
-
+                PrintHelpTopicNew();
             else if (Topic is HelpTopic.Serve)
-            {
-                Colorize("Description:", Gray, true);
-                Colorize("Make the contents of a projects site directory available at a given port. " +
-                    "It also watches the projects resources directory for changes. When a file is changed," +
-                    " renamed, created, moved or deleted, a build is triggered and the new contents of site " +
-                    "will automatically be available for viewing at the previously specified port.", White, true);
-                Colorize("Synopsis: ", Gray, true);
-                Colorize("serve ", White, false);
-                Colorize("[path] ", Gray, false);
-                Colorize("[port]", Gray, true);
-                Colorize("Example: ", Gray, true);
-                Colorize("stein serve ", White, false);
-                Colorize("./projects/mysite 8001", Gray, true);
-                Colorize("Options:", Gray, true);
-                Colorize("[path] ", White, false);
-                Colorize("(default: current directory)", DarkGray, true);
-                Colorize("An absolute or relative path to the project that is being served.", Gray, true);
-                Colorize("[port] ", White, false);
-                Colorize("(default: 8000)", DarkGray, true);
-                Colorize("Represents the desired port on which to make this project viewable.", Gray, true);
-            }
+                PrintHelpTopicServe();
             else if (Topic is HelpTopic.General)
-            {
-                Colorize("build ", White, false);
-                Colorize("[path]", Gray, true);
-                Colorize("new ", White, false);
-                Colorize("[path]", Gray, true);
-                Colorize("serve ", White, false);
-                Colorize("[path] [port]", Gray, true);
-                Colorize("help ", White, false);
-                Colorize("<command>", Gray, true);
-            }
+                PrintHelpTopicGeneral();
         }
 
         public enum HelpTopic
@@ -96,5 +37,75 @@ namespace Stein.Routines
         }
 
         private HelpTopic Topic { get; }
+
+        private void PrintHelpTopicBuild()
+        {
+            Colorize("Description:", Gray, true);
+            Colorize("Attempt to process all resources in a given project and output the "
+                + "results to the site directory.", White, true);
+            Colorize("Synopsis: ", Gray, true);
+            Colorize("build ", White, false);
+            Colorize("[path]", Gray, true);
+            Colorize("Example: ", Gray, true);
+            Colorize("stein build ", White, false);
+            Colorize("./projects/mysite", Gray, true);
+            Colorize("Options:", Gray, true);
+            Colorize("[path] ", White, false);
+            Colorize("(default: current directory)", DarkGray, true);
+            Colorize("An absolute or relative path to the project that is being built.", Gray, true);
+        }
+
+        private void PrintHelpTopicNew()
+        {
+            Colorize("Description:", Gray, true);
+            Colorize("Scaffold out resources and site directories, as well as a " +
+                "stein.json file in the root of the new project directory.", White, true);
+            Colorize("Synopsis: ", Gray, true);
+            Colorize("new ", White, false);
+            Colorize("[path] ", Gray, true);
+            Colorize("Example: ", Gray, true);
+            Colorize("stein new ", White, false);
+            Colorize("./projects/newsite", Gray, true);
+            Colorize("Options:", Gray, true);
+            Colorize("[path] ", White, false);
+            Colorize("(default: current directory)", DarkGray, true);
+            Colorize("An absolute or relative path to the desired location of the new project. " +
+                           "If the directory does not exist, it will be created.", Gray, true);
+        }
+
+        private void PrintHelpTopicServe()
+        {
+            Colorize("Description:", Gray, true);
+            Colorize("Make the contents of a projects site directory available at a given port. " +
+                "It also watches the projects resources directory for changes. When a file is changed," +
+                " renamed, created, moved or deleted, a build is triggered and the new contents of site " +
+                "will automatically be available for viewing at the previously specified port.", White, true);
+            Colorize("Synopsis: ", Gray, true);
+            Colorize("serve ", White, false);
+            Colorize("[path] ", Gray, false);
+            Colorize("[port]", Gray, true);
+            Colorize("Example: ", Gray, true);
+            Colorize("stein serve ", White, false);
+            Colorize("./projects/mysite 8001", Gray, true);
+            Colorize("Options:", Gray, true);
+            Colorize("[path] ", White, false);
+            Colorize("(default: current directory)", DarkGray, true);
+            Colorize("An absolute or relative path to the project that is being served.", Gray, true);
+            Colorize("[port] ", White, false);
+            Colorize("(default: 8000)", DarkGray, true);
+            Colorize("Represents the desired port on which to make this project viewable.", Gray, true);
+        }
+
+        private void PrintHelpTopicGeneral()
+        {
+            Colorize("build ", White, false);
+            Colorize("[path]", Gray, true);
+            Colorize("new ", White, false);
+            Colorize("[path]", Gray, true);
+            Colorize("serve ", White, false);
+            Colorize("[path] [port]", Gray, true);
+            Colorize("help ", White, false);
+            Colorize("<command>", Gray, true);
+        }
     }
 }
